94dd4fdce4508ecd37128b1d1b08ade4dd0b628c
Rafael <rafaelmoreiracvo@yahoo.com.br>
2022-05-18 21:27:44 -0300

    atualizando

---
+++ Jogos/jogo.py
@@ -0,0 +1,6 @@
+'''Com base no arquivo do Campeonato Brasileiro de 2003 a 2020 em anexo faça:
+a) Extraia todos os jogos que o seu time ganhou como visitante
+b) Extraia todos os jogos que o seu time ganhou como Mandante
+'''
+df = open("Jogos.txt", 'r')
+print(df.read())
